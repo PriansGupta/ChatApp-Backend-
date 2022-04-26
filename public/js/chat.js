29c035c7d111631f0eca853d2534aa5eda951fd1
Priyansh <priyanshg615@gmail.com>
2022-04-26 23:38:53 +0530

    Sending Messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,17 +1,5 @@
 const socket = io();
 
-// socket.on("xyz",(count)=>{
-//     console.log("Updated",count)
-// })
-
-// const bt=document.querySelector("#plus")
-
-// bt.addEventListener('click',()=>{
-//     console.log("clicked")
-
-//     socket.emit("increment")
-// })
-
 socket.on("message", (msg) => {
   console.log(msg);
 });
