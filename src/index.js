ce234f2be774a1df6a0cc616b9c651ce16f3d4a5
Priyansh <priyanshg615@gmail.com>
2022-04-26 18:46:08 +0530

    server setup

--- src/index.js
+++ src/index.js
@@ -6,3 +6,9 @@ const app = express();
 const port=process.env.PORT || 3000;
 
 const publicDirectoryPath=path.join(__dirname,"../public")
+
+app.use(express.static(publicDirectoryPath))
+
+app.listen(port,()=>{
+    console.log("Server is Up on "+port)
+})
