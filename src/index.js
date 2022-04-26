db165ea83435d0dbb055446e538dc9d267ff6300
Priyansh <priyanshg615@gmail.com>
2022-04-26 18:44:58 +0530

    server setup

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
+
+const port=process.env.PORT || 3000;
+
+const publicDirectoryPath=path.join(__dirname,"../public")
