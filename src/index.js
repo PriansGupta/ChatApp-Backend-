bc5a1e9d85454573f46051556a2ee60e8d421793
Priyansh <priyanshg615@gmail.com>
2022-04-26 23:39:09 +0530

    Sending Messages

--- src/index.js
+++ src/index.js
@@ -13,21 +13,14 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-
 io.on("connection", (socket) => {
   console.log("New WebSocket Connection");
 
-socket.emit("message","Welcome")
+  socket.emit("message", "Welcome");
 
-socket.on("send-msg",(msg)=>{
-    io.emit("message",msg)
-})
-//   socket.emit("xyz",count)
-
-//   socket.on("increment",()=>{
-//       count++;
-//       io.emit("xyz",count)
-//   })
+  socket.on("send-msg", (msg) => {
+    io.emit("message", msg);
+  });
 });
 
 server.listen(port, () => {
