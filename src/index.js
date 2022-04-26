895cf05bd0ab882e5c0e9410e07a6994828f71bb
Priyansh <priyanshg615@gmail.com>
2022-04-26 18:58:14 +0530

    socket.io

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const express = require("express");
+const http=require("http")
 
 const app = express();
+const server=http.createServer(app)
 
 const port=process.env.PORT || 3000;
 
@@ -9,6 +11,6 @@ const publicDirectoryPath=path.join(__dirname,"../public")
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log("Server is Up on "+port+"!")
 })
